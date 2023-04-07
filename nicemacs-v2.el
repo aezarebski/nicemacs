a0780f2f9253eb5958058c48e4ddfd5c8351e3f4
aezarebski <aezarebski@gmail.com>
2023-04-07 14:17:29 +1000

    update v2 draft

--- nicemacs-v2.el
+++ nicemacs-v2.el
@@ -17,7 +17,21 @@
 (defvar nice-resources-dir "~/Documents/nicemacs/resources"
   "The path to the nicemacs resources directory on this machine.")
 
-(require 'cl)
+(require 'cl-lib)
+
+;; Be evil
+;; -------
+
+(setq evil-want-keybinding ())
+
+(require 'evil)
+(require 'evil-leader)
+(require 'evil-collection)
+
+(evil-mode 1)
+(evil-leader-mode 1)
+(global-evil-leader-mode 1)
+(evil-collection-init)
 
 ;; Look stunning
 ;; =============
@@ -32,8 +46,28 @@
 
 (setq fill-column 70)
 
+(evil-leader/set-key "t l" 'display-fill-column-indicator-mode)
+
 (add-to-list `custom-theme-load-path "~/.emacs.d/themes/")
-(load-theme 'solarized-light t)
+(load-theme 'solarized-light-high-contrast t)
+
+(defun nice-toggle-themes ()
+  "Toggle between two themes: solarized-light-high-contrast and
+solarized-dark-high-contrast and adjust the comment face to one
+that is visible in both."
+  (interactive)
+  (if (eq (car custom-enabled-themes) 'solarized-light-high-contrast)
+      (progn
+        (disable-theme 'solarized-light-high-contrast)
+        (load-theme 'solarized-dark-high-contrast t)
+	(setq font-lock-comment-face '((t (:background "#207e7b" :foreground "#e4ecda")))))
+    (progn
+      (disable-theme 'solarized-dark-high-contrast)
+      (load-theme 'solarized-light-high-contrast t)
+      (setq font-lock-comment-face '((t (:background "#e4ecda" :foreground "#207e7b")))))))
+
+(evil-leader/set-key "t t" 'nice-toggle-themes)
+
 (tool-bar-mode -1)
 
 (global-hl-line-mode t)
@@ -61,27 +95,26 @@
 
 (setq inhibit-splash-screen t)
 
-;; Be evil
-;; =======
+(evil-leader/set-key "z j" 'text-scale-decrease)
+(evil-leader/set-key "z k" 'text-scale-increase)
 
-(setq evil-want-keybinding ())
-
-(require 'evil)
-(require 'evil-leader)
-(require 'evil-collection)
-
-(evil-mode 1)
-(evil-collection-init '(magit dired))
-
-;; Sensible keys
-;; -------------
+;; Be sensible
+;; ===========
 
 (global-evil-leader-mode)
 
 (evil-leader/set-leader "<SPC>")
 (evil-leader/set-key "<SPC>" 'execute-extended-command)
 
-(evil-leader/set-key "'" 'eshell)
+(evil-leader/set-key "q r" 'restart-emacs)
+(evil-leader/set-key "q q" 'save-buffers-kill-emacs)
+
+;; Shell stuff
+;; -----------
+
+(setq eshell-cmpl-ignore-case t)
+(evil-leader/set-key "s e" 'eshell)
+(evil-leader/set-key "s b" (lambda () (interactive) (ansi-term "/bin/bash")))
 
 ;; File stuff
 ;; ----------
@@ -108,19 +141,14 @@
 (evil-leader/set-key "w J" 'evil-window-move-very-bottom)
 (evil-leader/set-key "w K" 'evil-window-move-very-top)
 
+;; Consult the oracle
+;; ------------------
+
 (evil-leader/set-key "h s" 'apropos)
 (evil-leader/set-key "h d f" 'describe-function)
 (evil-leader/set-key "h d m" 'describe-mode)
 (evil-leader/set-key "h d k" 'describe-key)
 (evil-leader/set-key "h d v" 'describe-variable)
-
-(evil-leader/set-key "t l" 'display-fill-column-indicator-mode)
-
-(evil-leader/set-key "q r" 'restart-emacs)
-(evil-leader/set-key "q q" 'kill-emacs)
-
-(evil-leader/set-key "z j" 'text-scale-decrease)
-(evil-leader/set-key "z k" 'text-scale-increase)
 
 ;; Be virtuous
 ;; ===========
@@ -183,6 +211,38 @@ backup dictionary."
 (require 'magit)
 
 (evil-leader/set-key "g s" 'magit-status)
+
+(defmacro nice-canned-commit-message (fname cmessage key)
+  "Define a canned commit message function with an Evil key binding.
+
+  This macro takes in three arguments:
+  - FNAME: A string that will be used to construct the function name.
+  - CMESSAGE: A string that represents the canned commit message.
+  - KEY: A string that represents the keybinding for the function using the Evil leader.
+
+  The function created by this macro generates a commit message with a timestamp by
+  concatenating the specified CMESSAGE string with the current day and time. The commit
+  is created using `magit-commit-create`, which is invoked with the `--edit` option to
+  open the commit message in an editor. The function is bound to the Evil leader key
+  sequence `g c KEY`, where `KEY` is the specified key string.
+
+  Example usage:
+  (nice-canned-commit-message \"my-canned-commit\" \"Fix some bugs\" \"c\")"
+  `(progn
+     (defun ,(intern (format "nccm-%s" fname)) ()
+       "Generate a canned commit message with a timestamp."
+       (interactive)
+       (let ((commit-message (format "%s %s"
+                                     ,cmessage
+                                     (downcase (format-time-string "%A %l:%M %p")))))
+	 (magit-commit-create (list "--edit" (concat "-m \"" commit-message "\"")))))
+     (evil-leader/set-key ,(concat "g c " key) (intern ,(format "nccm-%s" fname)))))
+
+(nice-canned-commit-message review "update reading list" "r")
+(nice-canned-commit-message website "update website" "w")
+(nice-canned-commit-message journal "update journal" "j")
+
+(setq magit-display-buffer-function #'magit-display-buffer-fullframe-status-v1)
 
 ;; Emacs Lisp
 ;; ----------
@@ -446,7 +506,6 @@ file is being visited, then open the file using `find-file'."
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(font-lock-comment-delimiter-face ((t (:background "#e4ecda" :foreground "#207e7b" :slant normal))))
- '(font-lock-comment-face ((t (:background "#e4ecda" :foreground "#207e7b"))))
  '(show-paren-match ((t (:background "#ffb9a1" :foreground "#8e433d")))))
 
 (custom-set-variables
